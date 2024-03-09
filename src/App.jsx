df7148c9a6854fb6a9a4efb2f216951ca9d87f36
Arnau Ramos <arnauramos21@gmail.com>
2024-03-09 15:03:00 +0100

    done iteration 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import companies from './companies.json'
 import technologies from './technologies.json'
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CompanyPage from "./pages/CompanyPage";
+import TechnologyPage from "./pages/TechnologyPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React Stack Tracker</h1>
+      <Navbar />
     </div>
   );
 }
