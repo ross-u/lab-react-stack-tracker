c7d5395f7fecdee11398f2f88ef324775e8e40e0
Arnau Ramos <arnauramos21@gmail.com>
2024-03-09 15:11:39 +0100

    Done iteration 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import "./App.css";
-import companies from './companies.json'
-import technologies from './technologies.json'
+import companies from "./companies.json";
+import technologies from "./technologies.json";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import CompanyPage from "./pages/CompanyPage";
 import TechnologyPage from "./pages/TechnologyPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <Navbar />
+      <div className="App">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/company/:companySlug" element={<CompanyPage />}></Route>
+      <Route path="/tech/:slug" element={<TechnologyPage />}></Route>
+    </Routes>
     </div>
   );
 }
